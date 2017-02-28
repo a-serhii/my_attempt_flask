1621aca46457f9c7eb6ef0523b2f17693ea2eb26
Sergey <sabarovsky@yahoo.com>
2017-02-28 10:20:29 +0200

    use flask-moment

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, request, make_response, redirect, render_template
+from flask.ext.moment import Moment
 from flask_bootstrap import Bootstrap
+from datetime import datetime
+
 app = Flask(__name__)
 # Создаем экзэмпляр приложения
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 @app.route('/')  # Ассоциация между адресом url и функцией
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')  # Включим переменный компонент name
