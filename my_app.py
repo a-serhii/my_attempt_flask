ccd8ec5e3ac69221a3ee1b714aeae551c4d95cb3
Sergey <sabarovsky@yahoo.com>
2017-03-01 11:25:27 +0200

    add flash

--- my_app.py
+++ my_app.py
@@ -20,13 +20,16 @@ class NameForm(FlaskForm):
     submit = SubmitField('Submit')
 
 
-@app.route('/', methods=['GET', 'POST'])  # Ассоциация между адресом url и функцией
+@app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
         session['name'] = form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name') )
+    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'))
 
 
 @app.route('/user/<name>')  # Включим переменный компонент name
